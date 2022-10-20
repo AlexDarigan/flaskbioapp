e8bb4043300ba65b5f3f1e855b795c9f93b20dc3
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-10-20 13:36:46 +0100

    Add Page Functions

--- app.py
+++ app.py
@@ -8,5 +8,31 @@ def index():
     return render_template("index.html")
 
 
+@app.get("/cv")
+def cv():
+    return render_template("cv.html")
+
+
+@app.get("/home")
+def home():
+    # Redundant with index?
+    return render_template("home.html")
+
+
+@app.get("/interests")
+def interests():
+    return render_template("interests.html")
+
+
+@app.get("/personal")
+def personal():
+    return render_template("personal.html")
+
+
+@app.get("/tech")
+def tech():
+    return render_template("tech.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
