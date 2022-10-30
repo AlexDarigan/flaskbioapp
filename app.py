56c02a3edad65e19c7f4d5542212ee27ba454b95
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-10-30 17:04:39 +0000

    Add Formatter

--- app.py
+++ app.py
@@ -28,36 +28,44 @@ def personal():
 def tech():
     return render_template("tech.html")
 
+
 # Redirects
 @app.get("/wat")
 def wat():
     return redirect("https://github.com/alexdarigan/wat", 301)
 
+
 @app.get("/godotengine")
 def godotengine():
     return redirect("https://www.godotengine.org", 301)
+
 
 # Tech Sub Page Routes
 @app.get("/godot")
 def godot():
     return render_template("godot.html")
 
+
 # godot sub page routes
 @app.get("/juan")
 def juan():
     return redirect("https://www.linkedin.com/in/juan-linietsky-8a81203/", 301)
 
+
 @app.get("/ariel")
 def ariel():
     return redirect("https://www.linkedin.com/in/ariel-manzur-7b51263/", 301)
+
 
 @app.get("/compose")
 def compose():
     return render_template("compose.html")
 
+
 @app.get("/docker")
 def docker():
     return render_template("docker.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
